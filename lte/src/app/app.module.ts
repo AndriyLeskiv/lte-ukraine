a917a7ebe978fb0f150e288a9cf8a35033ca082f
Andriy Leskiv <andriy_leskiv@epam.com>
2018-05-17 17:00:21 +0300

    add amg

--- lte/src/app/app.module.ts
+++ lte/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { MapComponent } from './map/map.component';
-
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,12 @@ import { MapComponent } from './map/map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    FormsModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDZl6xpMX3UKRsWq8doSlkRAxo4tv0kyvA'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- lte/src/app/map/map.component.ts
+++ lte/src/app/map/map.component.ts
@@ -9,6 +9,33 @@ export class MapComponent implements OnInit {
 
   constructor() { }
 
+  title: string = 'Розташування Базових станцій';
+  lat: number = 48.8;
+  lng: number = 30.5;
+
+  public baseStation = [
+    {
+      lat: 48.8,
+      lng: 30.5,
+      rad: 5000
+    },
+    {
+      lat: 48.2,
+      lng: 30.5,
+      rad: 40000
+    },
+    {
+      lat: 48.5,
+      lng: 30.0,
+      rad: 10000
+    },
+    {
+      lat: 48.5,
+      lng: 30.2,
+      rad: 10000
+    }
+  ]
+
   ngOnInit() {
   }
 
